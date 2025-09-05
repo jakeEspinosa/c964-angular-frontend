ae01dc4b27c83ceaa2d01c3c79fcebd99e6e35e7
Jake Espinosa <jacob.sa.espinosa@gmail.com>
2025-09-04 20:01:32 -0700

    finish layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { AppHeader } from './header/header';
-import { MainContent } from './main-content/main-content';
+import { MainContent } from './views/main-content/main-content';
+import { AppFooter } from './footer/app-footer';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [AppHeader, MainContent],
+  imports: [AppHeader, MainContent, AppFooter],
 })
 export class App {}
